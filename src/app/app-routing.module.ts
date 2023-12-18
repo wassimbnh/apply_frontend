0d70525622dda99812bb531fe6cd913b71ab2914
wassimbnh <104159727+wassimbnh@users.noreply.github.com>
2023-12-18 14:47:15 +0100

    client signup form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './authentication/signup/signup.component';
 
 const routes: Routes = [
-  {path:"signup", component: SignupComponent}
+  {path:"auth/signup-user", component: SignupComponent}
 ];
 
 @NgModule({
